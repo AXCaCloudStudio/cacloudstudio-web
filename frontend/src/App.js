8fdaee8186faf8ab590c795a76e9ea8980d2ed3a
chengqi.li <chengqi.li@cacloudstudio.com>
2025-06-22 23:35:05 -0700

    Change text and logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CaCloudStudio home page.
+          Ca Cloud Studio, LLC home page.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CaCloudStudio github
+          Ca Cloud Studio, LLC github
         </a>
       </header>
     </div>
