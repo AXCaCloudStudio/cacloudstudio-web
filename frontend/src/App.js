e45d610c8a7a69dfc3add4bb786d44a1eacc486f
chengqi.li <chengqi.li@cacloudstudio.com>
2025-06-22 23:37:32 -0700

    Logistics change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ca Cloud Studio LLC home page.
+          CA Cloud Studio LLC home page.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ca Cloud Studio LLC github
+          CA Cloud Studio LLC github
         </a>
       </header>
     </div>
