4b4b0d3909237dd1a84727544f2d6a9e4ee8a99d
chengqi.li <chengqi.li@cacloudstudio.com>
2025-06-22 23:36:56 -0700

    Remove commas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ca Cloud Studio, LLC home page.
+          Ca Cloud Studio LLC home page.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ca Cloud Studio, LLC github
+          Ca Cloud Studio LLC github
         </a>
       </header>
     </div>
