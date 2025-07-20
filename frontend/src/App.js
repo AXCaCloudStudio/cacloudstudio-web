f69e71b698db357f82d201a2c919c663b373223c
claremore <15971375+claremore@user.noreply.gitee.com>
2025-07-19 23:20:26 -0700

    Add some initial mini games

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,39 @@
-import logo from './Logo.png';
+// src/App.js
+import React from 'react';
 import './App.css';
+import logo from './Logo.png';
+
+const games = [
+  { name: 'Crush Game', image: '/images/00001.png', link: '/games/00001/index.html' },
+  { name: 'Donkey Jump', image: '/images/00002.png', link: '/games/00002/index.html' },
+  { name: 'One Line Puzzle', image: '/images/00003.jpg', link: '/games/00003/index.html' },
+  { name: 'Fishing', image: '/images/00004.png', link: '/games/00004/index.html' },
+  { name: 'Fireworks', image: '/images/00005.jpg', link: '/games/00005/index.html' },
+  // Add more games here
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CA Cloud Studio LLC home page.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/AXCaCloudStudio"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            textDecoration: "underline",
-            textDecorationSkipInk: "none",
-            fontFamily: "Arial",
-          }}
-        >
-          github
-        </a>
+        <h1>CA Cloud Studio - Game Collection</h1>
       </header>
+
+      <main className="Game-grid">
+        {games.map((game, index) => (
+          <div className="Game-item" key={index}>
+            <a href={game.link}>
+              <img src={game.image} alt={game.name} className="Game-image" />
+            </a>
+            <a href={game.link} className="Game-title">{game.name}</a>
+          </div>
+        ))}
+      </main>
+
+      <footer className="App-footer">
+        <img src={logo} className="Footer-logo" alt="logo" />
+        <p>&copy; 2025 CA Cloud Studio LLC. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
